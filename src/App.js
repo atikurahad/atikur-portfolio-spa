df8e141d050cf81aeb6b0cb9fc5ac623f3261647
Atikur Rahaman <atikurahad@gmail.com>
2023-02-13 23:29:20 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
      <Navbar />
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h1> I am from footer</h1>
+      <div className='bg-[#1B4D3E] h-[13rem]'>
+        <div> hi</div>
+        <div>hello</div>
+        <div>hoe are you</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
