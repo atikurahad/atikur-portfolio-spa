9565cff6ad561dd7e9f55ddccdfa44a806f1c3a7
Atikur Rahaman <atikurahad@gmail.com>
2023-02-14 23:39:49 +0600

    footer up to date

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
       <h1> I am from footer</h1>
-      <div className='bg-[#1B4D3E] h-[13rem]'>
-        <div> hi</div>
-        <div>hello</div>
-        <div>hoe are you</div>
+      <div className="bg-[#1B4D3E] h-[13rem] flex">
+        <div className="flex-1">
+          <a href="/">Privacy Policy</a>
+          <a href="/">Terms & Conditions</a>
+          <a href="/">Our Achivement</a>
+          <a href="/">Security</a>
+        </div>
+        <div className="flex-1">
+          {" "}
+          <a href="/">All Projects</a>
+          <a href="/"> Get A Touch</a>
+          <a href="/">Contact</a>
+          <a href="/">About</a>{" "}
+        </div>
       </div>
+      <p>All rights reserved by It's owner</p>
     </div>
   );
 };
